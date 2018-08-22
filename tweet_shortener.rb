8a248818c1cbd5f18e1a25fd320e639858dc7ee2
Andrew Sprague <andrewsprague@outlook.com>
2018-08-22 13:41:51 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,6 +13,27 @@ dictionary = {
   "and" => '&'
 }
 
-def word_substituter (tweet, dictionary)
-  puts dictionary
+def word_substituter (tweet)
+  dictionary = {
+    "hello" => 'h1',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => '@',
+    "and" => '&'
+  }
+
+  tweet.split(" ").each do |word|
+    dictionary.each do |long_word, short_word|
+      if word == long_word
+        puts short_word
+      else
+        puts word
+      end
+    end
+  end  
 end
