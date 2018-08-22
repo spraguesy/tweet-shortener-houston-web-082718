7afbf1ac0593a785253b8e1d1479e9e5fdb788c7
Andrew Sprague <andrewsprague@outlook.com>
2018-08-22 13:11:01 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+dictionary = {
+  "hello" => 'h1',
+  "to" => '2',
+  "two" => '2',
+  "too" => '2',
+  "for" => '4',
+  "four" => '4',
+  "be" => 'b',
+  "you" => 'u',
+  "at" => '@',
+  "and" => '&'
+}
+
+def word_substituter (sting)
+  new_string = []
+  string.each do |word|
+    dictionary.each do |key, value|
+      if word == key
+        new_string.push(value)
+      else
+        new_string.push(word)
+      end
+        
+end
