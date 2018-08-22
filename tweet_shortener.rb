9ba462d66beb1069e72ff5aed35014e0d6eb381a
Andrew Sprague <andrewsprague@outlook.com>
2018-08-22 14:27:04 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -53,6 +53,7 @@ end
 def shortened_tweet_truncator (tweet)
   new_tweet = ''
   if word_substituter(tweet).length > 140
+    puts tweet
     tweet.each_with_index do |char, index|
         if index <= 140
           new_tweet = new_tweet + char
